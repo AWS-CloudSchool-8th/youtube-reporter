6c8fb638f9b5d00cf7c685aea17068db12bfbf46
EC2 Default User <ec2-user@ip-172-31-23-2.ec2.internal>
2025-07-01 12:51:12 +0000

    feat(auth): solve cors in main

--- app/auth_service/main.py
+++ app/auth_service/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.auth import router as auth_router
 
 app = FastAPI(
     title="Auth Service",
     description="Handles user authentication with AWS Cognito",
     version="1.0.0"
+    # ? root_path는 넣지 않음
+)
+
+# ? CORS 허용
+origins = [
+    "http://34.228.65.221:3000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(auth_router)
-# trigger test
